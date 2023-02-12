0a7c93aef28aa3a1036c07a36acb65d7dc93a254
dargall <dargall91@comcast.net>
2023-02-12 11:16:33 -0500

    Update combat endpoints

--- src/main/java/com/server/controllers/CombatController.java
+++ src/main/java/com/server/controllers/CombatController.java
@@ -1,5 +1,6 @@
 package com.server.controllers;
 
+import com.server.repositories.projections.NameIdProjection;
 import com.server.util.CampaignManager;
 import com.server.entities.encounter.Encounter;
 import com.server.models.Combatant;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,8 +21,7 @@ import java.util.Optional;
 public class CombatController {
     @Autowired
     private EncounterRepository encounterRepo;
-    private Encounter loadedEncounter;
-    private List<Combatant> combatantList;
+    private List<NameIdProjection> combatantList = new ArrayList<>();
 
     @GetMapping("/")
     public String combatView(Model model) {
@@ -35,36 +36,16 @@ public class CombatController {
         return "combat";
     }
 
-    @PostMapping("load")
-    public ResponseEntity<?> loadEncounter(@RequestParam int encounterId) {
-        Optional<Encounter> encounter = encounterRepo.findById(encounterId);
-
-        if (encounter.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Encounter with id " + encounterId + " not found.");
-        }
-
-        loadedEncounter = encounter.get();
-
-        return ResponseEntity.noContent().build();
-    }
-
     @PostMapping("setCombatants")
-    public ResponseEntity<?> setCombatanats(@RequestBody List<Combatant> combatants, Model model) {
+    public ResponseEntity<?> setCombatanats(@RequestBody List<NameIdProjection> combatants, Model model) {
         combatantList = combatants;
 
-        for (int i =0 ; i < combatantList.size(); i++) {
-            combatantList.get(i).setId(i + 1);
-        }
-
         return ResponseEntity.noContent().build();
     }
 
-    /**
-     * Gets the loaded encounter. WIll be null if no encounter is loaded
-     * @return
-     */
-    @GetMapping("loaded")
-    public ResponseEntity<?> getLoadedEncounter() {
-        return ResponseEntity.ok(loadedEncounter);
+    @PostMapping("endCombat")
+    public ResponseEntity<?> endCombat() {
+        combatantList.clear();
+        return ResponseEntity.noContent().build();
     }
 }
