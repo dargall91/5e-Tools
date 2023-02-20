5b035970e3e88c91085a49808c2d9c9c61286eb0
dargall <dargall91@comcast.net>
2023-02-19 19:50:40 -0500

    Implement CForm and Axios for login

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,19 +1,15 @@
 <template>
   <NavigationComponent />
   <LoginForm />
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import NavigationComponent from './components/NavigationComponent.vue';
 import LoginForm from './components/LoginForm.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
     NavigationComponent,
     LoginForm
   }
@@ -25,7 +21,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
--- front-end/src/components/LoginForm.vue
+++ front-end/src/components/LoginForm.vue
@@ -1,20 +1,53 @@
 <template>
-    <form class="container container">
-        <div class="form-group">
-            <label for="username">Username</label>
-            <input type="email" class="form-control" id="username" aria-describedby="emailHelp" placeholder="Username">
-        </div>
-        <div class="form-group">
-            <label for="password">Password</label>
-            <input type="password" class="form-control" id="password" aria-describedby="passwordHelp"  placeholder="Password">
-            <small id="passwordHelp" class="form-text text-muted">I don't know how to encypt a password yet, so...</small>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    <div class="container">
+        <h2 class="mt-2">Login</h2>
+        <CForm @submit.prevent="submit" method="POST">
+            <CFormFloating class="mb-3 mt-3">
+                <CFormInput v-model="loginForm.username" required type="username" class="form-control" id="username" placeholder="Username" />
+                <CFormLabel for="username">Username</CFormLabel>
+            </CFormFloating>
+
+            <CFormFloating class="mb-3">
+                <CFormInput v-model="loginForm.password" required type="password" class="form-control" id="password" placeholder="Password" />
+                <CFormLabel for="password">Password</CFormLabel>
+            </CFormFloating>
+
+            <CFormCheck class="mb-3" label="Remember Me" type="checkbox" name="remember" />
+
+            <CButton type="submit" class="btn btn-primary">Submit</CButton>
+        </CForm>
+    </div>
 </template>
 
 <script>
+    import { CFormFloating, CForm, CFormInput, CFormLabel, CFormCheck, CButton } from '@coreui/vue'
+    import axios from 'axios'
+
     export default {
-        name: 'LoginForm'
+        name: "LoginForm",
+        components: { CFormFloating, CForm, CFormInput, CFormLabel, CFormCheck, CButton },
+        data() {
+            return {
+                loginForm: {
+                    username: "",
+                    password: ""
+                },
+                result: {
+                    username: "",
+                    password: ""
+                },
+
+            }
+        },
+        methods: {
+            submit() {
+                axios.post('http://localhost:8080/5eTools/api/user/login', this.loginForm)
+                .then(response => {
+                    this.result.username = response.data.username
+                    this.result.password = response.data.password
+                })
+                .catch(error => console.log(error.message));
+            }
+        }
     }
 </script>
